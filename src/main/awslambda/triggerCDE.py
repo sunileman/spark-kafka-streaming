bd15524217b8907e2ffceaa04d5c69b1061655b5
Sunile Manjee <sunilemanjee@gmail.com>
2020-09-11 13:58:09 -0500

    added new CDE simple run

---
+++ src/main/awslambda/triggerCDE.py
@@ -0,0 +1,54 @@
+import json
+import urllib.parse
+import boto3
+import os
+import json
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+
+
+
+    user="YOUR-CDP-WORKLOAD-USER"
+    passwd="YOUR-CDP-WORLOAD-PASS"
+    userpasswd=user+":"+passwd
+
+    grafanaurl="https://service.cde-2sp6d49p.sunman1.a465-9q4k.cloudera.site/grafana/d/0Oq0WmQWk/instance-metrics?orgId=1&refresh=5s&var-virtual_cluster_name=etl"
+    jobsurl="\"https://9b9wwrgp.cde-2sp6d49p.sunman1.a465-9q4k.cloudera.site/dex/api/v1/jobs/testjob/run\" "
+    jobpayload="\"{\\\"overrides\\\":{\\\"spark\\\":{\\\"args\\\":[\\\"hello\\\"]}},\\\"user\\\":\\\""+user+"\\\"}\""
+    getToken="curl -u \""+userpasswd+"\" $(echo \'"+grafanaurl+"\' | cut -d\'/\' -f1-3 | awk \'{print $1\"/gateway/authtkn/knoxtoken/api/v1/token\"}\')"
+
+    print("curling to get cde token")
+
+    stream= os.popen(getToken)
+    json_object_string = stream.read()
+    json_object = json.loads(json_object_string)
+    cde_token=json_object["access_token"]
+    #print("Access Token: "+cde_token)
+
+
+    #build job url
+    jobsubmit="curl -b hadoop-jwt="+cde_token+" -X POST "+jobsurl+" -H \"Content-Type: application/json\" -d "+jobpayload
+
+    print("call cde for spark submit")
+    jobstream = os.popen(jobsubmit)
+    joboutput = jobstream.read()
+    print("CDE JobID: "+joboutput)
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        print("bucket: "+bucket)
+        print("key: "+key)
+        return response['ContentType']
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
---
+++ src/main/scala/SimpleCDERun.scala
@@ -0,0 +1,29 @@
+package com.cloudera.examples
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+import scala.io.Source
+
+object SimpleCDERun {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder
+      .appName("Simple CDE Run Example")
+      .getOrCreate()
+
+    import spark.implicits._
+
+
+    val imputmessage = args(0)
+
+    println("\n*******************************")
+    println("\n*******************************")
+    println("source topic: "+imputmessage)
+    println("\n*******************************")
+    println("\n*******************************")
+
+
+
+  }
+}
