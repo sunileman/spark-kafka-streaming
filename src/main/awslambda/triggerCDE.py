260944c25958f94e3e37bb1ff91a1b0cdbbfb1d2
Sunile Manjee <sunilemanjee@gmail.com>
2020-11-10 08:31:21 -0600

    updated aws lambda trigger

--- src/main/awslambda/triggerCDE.py
+++ src/main/awslambda/triggerCDE.py
@@ -1,78 +1,69 @@
 import json
 import urllib.parse
 import boto3
-import os
-import json
+import urllib3
+import uuid
 
 print('Loading function')
 
 s3 = boto3.client('s3')
+grafanaurl = "https://<grafanaurl from console>/gateway/authtkn/knoxtoken/api/v1/token"
+jobsurl = "https://<jobs url from console>/dex/api/v1/jobs/jobname/run"
+arg1 = "<any argument>"
+arg2 = "<any argument>"
+cde_uuid = str(uuid.uuid1())
+
+def get_secret():
+    """Retrieve the CDE credentials from AWS Secrets Manager."""
+    client = boto3.client('secretsmanager')
+    try:
+        response = client.get_secret_value(
+            SecretId='your-service-id'
+        )
+    except ClientError as e:
+        print('Failed to retrieve secret from Secrets Manager.')
+        print(e)
+        exit()
+    else:
+        secret = json.loads(response['SecretString'])
+        return '{0}:{1}'.format(secret['username'], secret['password'])
+
+def getToken(grafanaurl):
+    creds = get_secret()
+    http = urllib3.PoolManager()
+    headers = urllib3.make_headers(basic_auth=creds)
+    req_token = http.request('GET', grafanaurl, headers=headers)
+
+    token = json.loads(req_token.data.decode('utf-8'))
+    return token['access_token']
+
+def submitjob(cde_token, jobsurl, jobpayload):
+    http = urllib3.PoolManager()
+    headers = {"Content-Type": "application/json",
+               "Authorization": "Bearer " + cde_token}
+    encodedpayload = json.dumps(jobpayload).encode('utf-8')
+    r_jobsubmit = http.request('POST', jobsurl, headers=headers, body=encodedpayload)
+    print(r_jobsubmit.status)
+    print(dir(r_jobsubmit))
 
 
 def lambda_handler(event, context):
 
-
-
-    user="YOUR-CDP-WORKLOAD-USER"
-    passwd="YOUR-CDP-WORLOAD-PASS"
-    userpasswd=user+":"+passwd
-
-    grafanaurl="<Your Grafana URL>"
-    jobsurl="\"<Your jobs url>\" "
-    #grafanaurl="https://service.cde-2sp6d49p.sunman1.a465-9q4k.cloudera.site/grafana/d/0Oq0WmQWk/instance-metrics?orgId=1&refresh=5s&var-virtual_cluster_name=etl"
-    #jobsurl="\"https://9b9wwrgp.cde-2sp6d49p.sunman1.a465-9q4k.cloudera.site/dex/api/v1/jobs/testjob/run\" "
-
-    #example
-    #jobpayload="\"{\\\"overrides\\\":{\\\"spark\\\":{\\\"args\\\":[\\\"hello\\\"]}},\\\"user\\\":\\\""+user+"\\\"}\""
-    jobpayloadpre="\"{\\\"overrides\\\":{\\\"spark\\\":{\\\"args\\\":[\\\""
-    jobpayloadpost="\\\"]}},\\\"user\\\":\\\""+user+"\\\"}\""
-    getToken="curl -u \""+userpasswd+"\" $(echo \'"+grafanaurl+"\' | cut -d\'/\' -f1-3 | awk \'{print $1\"/gateway/authtkn/knoxtoken/api/v1/token\"}\')"
-
-    print("curling to get cde token")
-
-    stream= os.popen(getToken)
-    json_object_string = stream.read()
-    json_object = json.loads(json_object_string)
-    cde_token=json_object["access_token"]
-    #print("Access Token: "+cde_token)
-
-
-    ##example
-    ##{
-    ##  "overrides": {
-    ##    "spark": {
-    ##      "args": [
-    ##        "arg1",
-    ##        "arg2",
-    ##       "arg3"
-    ##      ]
-    ##    }
-    ##  },
-    ##  "user": "srv_cde-cbraw-job"
-    ##}
+    print("Function Star")
 
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        print("bucket: "+bucket)
-        print("key: "+key)
-        jobpayload=jobpayloadpre+bucket+"/"+key+jobpayloadpost
-        print("jobpayload: "+jobpayload)
+    payload_dict = {
+    "overrides": {
+        "spark": {"args": ['s3a://{}/{}'.format(bucket,key),
+                           arg1,
+                           arg2]
+                  }
+        },
+    "variables": {"uuid": cde_uuid }
+    }
 
-        #build job url
-        jobsubmit="curl -H \"Authorization: Bearer "+cde_token+" -X POST "+jobsurl+" -H \"Content-Type: application/json\" -d "+jobpayload
-        # for v1 jobsubmit="curl -b hadoop-jwt="+cde_token+" -X POST "+jobsurl+" -H \"Content-Type: application/json\" -d "+jobpayload
+    cde_token = getToken(grafanaurl)
 
-        print("call cde for spark submit")
-        jobstream = os.popen(jobsubmit)
-        joboutput = jobstream.read()
-        print("CDE JobID: "+joboutput)
-
-        return response['ContentType']
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+    submitjob(cde_token, jobsurl, payload_dict)
